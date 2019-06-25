f5068c1127f84f9815ac38957dd1b3ca8a530626
ysumeng7 <ysu_m@hotmail.com>
2019-06-25 11:06:51 +0800

    first commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,74 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import MagicDropzone from "react-magic-dropzone";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import * as mobilenet from "@tensorflow-models/mobilenet";
+import "@tensorflow/tfjs";
+import "./styles.css";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class App extends React.Component {
+  state = {
+    model: null,
+    preview: "",
+    predictions: []
+  };
+
+  componentDidMount() {
+    mobilenet.load().then(model => {
+      this.setState({
+        model: model
+      });
+    });
+  }
+
+  onDrop = (accepted, rejected, links) => {
+    this.setState({ preview: accepted[0].preview || links[0] });
+  };
+
+  onImageChange = e => {
+    this.state.model.classify(e.target).then(predictions => {
+      this.setState({ predictions: predictions });
+    });
+  };
+
+  render() {
+    return (
+      <div className="Dropzone-page">
+        {this.state.model ? (
+          <MagicDropzone
+            className="Dropzone"
+            accept="image/jpeg, image/png, .jpg, .jpeg, .png"
+            multiple={false}
+            onDrop={this.onDrop}
+          >
+            <div className="Dropzone-content">
+              {this.state.preview ? (
+                <img
+                  alt="upload preview"
+                  onLoad={this.onImageChange}
+                  className="Dropzone-img"
+                  src={this.state.preview}
+                />
+              ) : (
+                "Choose or drop a file."
+              )}
+            </div>
+          </MagicDropzone>
+        ) : (
+          <div className="Dropzone">Loading model...</div>
+        )}
+        <div>
+          {this.state.predictions.map(item => (
+            <div className="prediction" key={item.className}>
+              <div>{item.className}</div>
+              <div>{item.probability.toFixed(2)}</div>
+            </div>
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
